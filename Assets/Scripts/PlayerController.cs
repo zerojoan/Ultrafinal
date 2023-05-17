6990f50bd670cb532ffb7e4314692dd3159ee967
zerojoan <zerojoan117@gmail.com>
2023-05-17 12:01:51 +0200

    caminar y saltar porfa guardate

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public float runSpeed=2;
+    
+    public float jumpSpeed = 3;
+
+    Rigidbody2D rb2D;
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2D.velocity= new Vector2(runSpeed, rb2D.velocity.y);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2D.velocity= new Vector2(-runSpeed, rb2D.velocity.y);
+        }
+        else
+        {
+            rb2D.velocity=new Vector2(0, rb2D.velocity.y);
+        }
+        if (Input.GetKey("space") && CheckGround.isGrounded)
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+        }
+        
+    }
+}
