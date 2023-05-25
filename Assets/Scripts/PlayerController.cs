6cfff063bece2670f6125b007337b0830f2b4ff8
zerojoan <zerojoan117@gmail.com>
2023-05-25 10:50:05 +0200

    si

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,38 @@ public class PlayerController : MonoBehaviour
     public float runSpeed=2;
     
     public float jumpSpeed = 3;
+    public spriteRenderer spriteRenderer;
+    private Rigidbody2D rBody;
+    private GroundSensor sensor;
+    float horizontal;
 
     Rigidbody2D rb2D;
     void Start()
     {
+        spriteRenderer = GetComponent<spriteRenderer>();
         rb2D = GetComponent<Rigidbody2D>();
+        sensor = GameObject.Find("GroundSensor").GetComponent<GroundSensor>(); 
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        
+        if(horizontal < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        else if(horizontal > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        if(Input.GetButtonDown("Jump") && sensor.isGrounded)
+        {
+            rBody.AddForce(vector2.up * jumpforce, ForceMode2D.impulse);
+        }    
+        
     }
 
     
